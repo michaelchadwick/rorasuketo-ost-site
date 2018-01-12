35a4dbc0a8f987c8dc9398c129341b9e6851395e
Michael Chadwick <mchadwick@ucsd.edu>
2018-01-12 15:34:31 -0800

    fixed playlist

--- content/scripts/playlist.js
+++ content/scripts/playlist.js
@@ -1,37 +1,43 @@
 /* global $ */
 
 $(function () {
+  var current = 0
+  var player = $('audio')[0]
+  var playlist = $('#playlist')
+  var tracks = playlist.find('li a')
+  var len = tracks.length - 1
+  var track = null
+
   init()
 
   function init () {
-    var current = 0
-    var audio = $('audio')
-    var playlist = $('#playlist')
-    var tracks = playlist.find('li a')
-    var len = tracks.length - 1
-    audio[0].volume = 1.0
-    // audio[0].play()
+    // set initial volume
+    player.volume = 1.0
+
     playlist.on('click', 'a', function (e) {
       e.preventDefault()
-      var link = $(this)
-      current = link.parent().index()
-      run(link, audio[0])
+      track = $(this)
+      current = track.parent().index()
+      play(track)
     })
-    audio[0].addEventListener('ended', function (e) {
+
+    player.addEventListener('ended', function (e) {
       current++
+
       if (current === len) {
         current = 0
-        var link = playlist.find('a')[0]
+        track = playlist.find('a')[0]
       } else {
-        link = playlist.find('a')[current]
+        track = playlist.find('a')[current]
       }
-      run($(link), audio[current])
+
+      play($(track))
     })
   }
-  function run (link, player) {
-    player.src = link.attr('href')
-    var par = link.parent()
-    par.addClass('active').siblings().removeClass('active')
+
+  function play (track) {
+    player.src = track.attr('href')
+    track.parent().addClass('active').siblings().removeClass('active')
     player.load()
     player.play()
   }

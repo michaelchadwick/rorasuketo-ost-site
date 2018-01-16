3cd6e3855e60c43bdfc590c3efd21893cc31863f
Michael Chadwick <mchadwick@ucsd.edu>
2018-01-16 10:24:28 -0800

    tracks now in japanese; added rew and fwd buttons; rebounced some tracks

--- content/scripts/playlist.js
+++ content/scripts/playlist.js
@@ -3,6 +3,8 @@
 $(function () {
   var current = 0
   var player = $('audio')[0]
+  var rewind = $('#rewind')
+  var forward = $('#forward')
   var playlist = $('#playlist')
   var tracks = playlist.find('li a')
   var len = tracks.length - 1
@@ -11,7 +13,6 @@ $(function () {
   init()
 
   function init () {
-    // set initial volume
     player.volume = 1.0
 
     playlist.on('click', 'a', function (e) {
@@ -21,18 +22,32 @@ $(function () {
       play(track)
     })
 
-    player.addEventListener('ended', function (e) {
-      current++
-
-      if (current > len) {
-        current = 0
-        track = playlist.find('a')[0]
-      } else {
-        track = playlist.find('a')[current]
-      }
-
-      play($(track))
+    rewind.on('click', function (e) {
+      e.preventDefault()
+      current = current - 1
+      changeTrack(current)
     })
+
+    forward.on('click', function (e) {
+      e.preventDefault()
+      current = current + 1
+      changeTrack(current)
+    })
+
+    player.addEventListener('ended', function () {
+      current = current + 1
+      changeTrack(current)
+    })
+  }
+
+  function changeTrack (current) {
+    if (current > len) {
+      current = 0
+    } else if (current < 0) {
+      current = len
+    }
+
+    play($(playlist.find('a')[current]))
   }
 
   function play (track) {
